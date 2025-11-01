5ad43d01449610a59baf780dbf5658f50e145ff6
asceznyk <asceznyk@gmail.com>
2025-11-01 15:23:42 +0530

    added prefix and ctrl-c support on client side

--- client.py
+++ client.py
@@ -1,37 +1,51 @@
 import time
 import socket
 import threading
+import traceback
+import sys
+from contextlib import suppress
 
 def recieve_msgs(client:socket.socket):
-  while True:
+  while not stop_event.is_set():
     try:
       msg = client.recv(1024).decode('utf-8')
-      if msg == '[EXIT]':
-        client.close()
-        break
+      if msg == '[EXIT]': continue
       print(f"other: {msg}")
     except:
-      pass
+      print(f"error = {traceback.format_exc()}")
+      stop_event.set()
   return
 
 def send_msg(client:socket.socket):
-  while True:
-    msg = input("")
-    client.sendall(msg.encode('utf-8'))
-    if msg == '[EXIT]':
-      break
-  client.close()
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 5000))
-threading.Thread(
-  target=recieve_msgs,
-  args=(client,),
-  daemon=True
-).start()
+  while not stop_event.is_set():
+    try:
+      msg = input()
+      client.sendall(msg.encode('utf-8'))
+      if msg == '[EXIT]':
+        stop_event.set()
+        break
+      sys.stdout.write("\033[F")
+      sys.stdout.write("\r\033[K")
+      sys.stdout.flush()
+      print(f"you: {msg}")
+    except KeyboardInterrupt:
+      print("exiting..")
+      client.sendall("[EXIT]".encode('utf-8'))
+      stop_event.set()
+    except:
+      print(f"Exception: {traceback.format_exc()}")
+  return
 
 if __name__ == "__main__":
+  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client.connect(('127.0.0.1', 5000))
+  stop_event = threading.Event()
   print(f"client connected to server {client}")
+  threading.Thread(
+    target=recieve_msgs,
+    args=(client,),
+    daemon=True
+  ).start()
   send_msg(client)
 
 
--- server.py
+++ server.py
@@ -1,51 +1,65 @@
-from typing import Tuple
+import sys
 import time
 import socket
 import threading
 import traceback
 from queue import Queue
+from typing import Tuple
 
 open_clients = {}
 messages = Queue()
+stop_event = threading.Event()
 
 def recieve_msgs(conn:socket.socket, addr:Tuple[str,int]):
-  while True:
-    try:
-      msg = conn.recv(1024).decode('utf-8')
-      if msg == '[EXIT]':
-        conn.close()
-        del open_clients[addr]
-        print(f"Client disconnected open_clients = {open_clients}")
-        break
-      messages.put((conn, msg))
-    except:
-      print(f"error occured = {traceback.format_exc()}")
-  return 
+  while not stop_event.is_set():
+    msg = conn.recv(1024)
+    if not msg: continue 
+    msg = msg.decode('utf-8')
+    if msg == '[EXIT]':
+      conn.close()
+      del open_clients[addr]
+      print(f"Client disconnected... ")
+      print(f"open_clients = {open_clients}")
+      stop_event.set()
+    messages.put((conn, msg)) 
 
 def broadcast_msg():
-  while True:
-    try: 
-      sender, msg = messages.get()
-      for addr, client in open_clients.items():
-        if client != sender:
-          client.sendall(msg.encode('utf-8'))
-    except:
-      print(f"error = {traceback.format_exc()}")
-      break
-  return
+  while not stop_event.is_set():
+    sender, msg = messages.get()
+    for addr, client in open_clients.items():
+      try:
+        if client == sender: continue
+        client.sendall(msg.encode('utf-8'))
+      except (BrokenPipeError, ConnectionResetError):
+        del open_clients[addr]
+        client.close()
+      except:
+        print(f"error = {traceback.format_exc()}")
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 5000))
-server.listen()
+def main(server:socket.socket):
+  while not stop_event.is_set():
+    try: 
+      conn, addr = server.accept()
+      open_clients[addr] = conn
+      print(f"open_clients = {open_clients}")
+      threading.Thread(
+        target=recieve_msgs,
+        args=(conn,addr,),
+        daemon=True
+      ).start()
+    except KeyboardInterrupt:
+      if len(open_clients):
+        print("cannot close server, clients exist")
+      else:
+        print(f"exiting because, open_clients = {open_clients}")
+        stop_event.set()
 
 if __name__ == "__main__":
+  server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+  server.bind(('127.0.0.1', 5000))
+  server.listen()
   print("server listening on 127.0.0.1:5000")
   threading.Thread(target=broadcast_msg, daemon=True).start()
-  while True:
-    conn, addr = server.accept()
-    open_clients[addr] = conn
-    print(f"open_clients = {open_clients}")
-    threading.Thread(
-      target=recieve_msgs, args=(conn,addr,), daemon=True
-    ).start() 
+  main(server)
 
